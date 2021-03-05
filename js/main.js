4480e526b0b27cc769a3be9fa235a0cb6d4863fd
Xavier Torres-Casso <xavier@Xaviers-Air.charter.com>
2021-03-05 15:17:34 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*----- constants -----*/
+const cards
+const rank
+/*----- app's state (variables) -----*/
+let tally, scores, winner, draws
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+function shuffleDeck() {}
+
+function splitCards() {}
+
+function compareDraw() {}
+
+function 
